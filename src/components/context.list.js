51fd7b7055c254ffa8a04fb6d4b773d2ee34e0ea
ciacelo <marcelo.martins@dce.ufpb.br>
2019-07-14 22:47:19 -0300

    Estilizando cards

--- src/components/context.list.js
+++ src/components/context.list.js
@@ -1,29 +1,49 @@
 import React,{ Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { CardComp } from './components';
+import { CardB, CardDeckB, ButtonB } from './components';
 import './style/style.css';
 
-const { Meta } = CardComp;
+//const { Meta } = CardComp;
 
 function ContextList(props){
 
     let contexts = props.contexts.map((context)=>{
         return(
-            <CardComp
-                className = "card"
+            /*<ColComp   
+                span={7}                              
                 key={context.id}
-                onClick={()=>props.selectContext(context)}
-                cover={<img className="image" src={context.image} alt={context.name} />}>
-                <Meta 
-                    title={context.name}/>
-            </CardComp>
+                >
+                <CardComp 
+                    className = "card"
+                    onClick={()=>props.selectContext(context)}
+                    cover={<img className="image" src={context.image} alt={context.name} />}>
+                    <Meta 
+                        title={context.name}/>
+                </CardComp>
+            </ColComp>*/
+
+            <CardB
+                key={context.id}
+                onClick={()=>props.selectContext(context)}>
+                <CardB.Img variant="top" src={context.image} />
+                <CardB.Body>
+                    <CardB.Text>
+                        {context.name}
+                    </CardB.Text>
+                    <ButtonB variant="dark"><i class="fa fa-arrow-right" aria-hidden="true"></i></ButtonB>
+                    <ButtonB variant="dark">Dark</ButtonB>
+                </CardB.Body>
+            </CardB>
+            
         );
     });
 
     return(
         <Fragment>
-            {contexts}
-        </Fragment>
+            <CardDeckB>
+                {contexts}
+            </CardDeckB>        
+        </Fragment>   
     );
 }
 
